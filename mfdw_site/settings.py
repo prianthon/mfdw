a84d94faa89f7107dd014721b821e937b03f773e
Django <djangopython@MacBook-Pro-Pri.local>
2018-07-30 21:07:41 +0700

    template static hardcode

--- mfdw_site/settings.py
+++ mfdw_site/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mfdw_site/static'),
+]
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'mfdw_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'mfdw_site/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("<h1>The Meandco Homepage</h1>")
+	#return HttpResponse("<h1>The Meandco Homepage</h1>")
+	return render(request, 'pages/page.html')
