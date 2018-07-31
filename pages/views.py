689dbb6f52772a1c504026ad896e322c9a7ee3b2
Django <djangopython@MacBook-Pro-Pri.local>
2018-07-31 07:16:27 +0700

    content dynamic

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from . models import Page
 
-def index(request):
+def index(request, pagename):
 	#return HttpResponse("<h1>The Meandco Homepage</h1>")
-	return render(request, 'pages/page.html')
+	pagename = '/' + pagename
+	pg = Page.objects.get(permalink=pagename)
+	context = {
+		'title': pg.title,
+		'content': pg.bodytext,
+		'last_updated': pg.update_date,
+	}
+	#assert False
+	return render(request, 'pages/page.html', context)
