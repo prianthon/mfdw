81c844d0724514e15dc9f69b671a46f8a3d31357
Django <djangopython@MacBook-Pro-Pri.local>
2018-08-30 18:42:48 +0700

    halaman 158 MFDW

--- mfdw_site/settings.py
+++ mfdw_site/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'quotes.apps.QuotesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
